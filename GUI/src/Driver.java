cae039834e5cc9b15bf0a3b75d2301800b3507c1
Healveon <chong2744sdsu@gmail.com>
2022-03-25 08:52:47 -0700

    Creates directory for files
    Creates a directory for the files that the listing app uses

--- GUI/src/Driver.java
+++ GUI/src/Driver.java
@@ -4,10 +4,17 @@ public class Driver {
 
     public static void main (String[] args) {
 
-        //Creates output and save files
-        File output = new File("outputs.txt");  //saves user output (event)
-        File score = new File("score.txt");     //saves scores
-        File dates = new File("dates.txt");     //saves dates
+        //Create directory within current directory if it does not exist
+        boolean dirSuccess = true;
+        if (!(new File(new File(".").getAbsolutePath() + "/ListApp").exists())) 
+            dirSuccess = new File(new File(".").getAbsolutePath() + "/ListApp").mkdirs();
+        
+        //Creates and adds file if directory was successfully created or exists 
+        //Exits otherwise
+        if (!dirSuccess) { System.out.println("Failed to create directory for ListApp"); System.exit(0); }
+        File output = new File(new File(".").getAbsolutePath() + "/ListApp/outputs.txt");  //saves user output (event)
+        File score = new File(new File(".").getAbsolutePath() + "/ListApp/score.txt");     //saves scores
+        File dates = new File(new File(".").getAbsolutePath() + "/ListApp/dates.txt");     //saves dates
         fileModifier.createFile(output);
         fileModifier.createFile(score);
         fileModifier.createFile(dates);
